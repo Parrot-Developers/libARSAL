5773dfa9674d96b91c5f89f0e16fa98a45c25772
Frédéric D'HAEYER <frederic.dhaeyer@parrot.com>
2012-05-15 13:46:22 +0200

    Rename libsal files.

---
+++ Includes/libSAL/libsal.h
@@ -0,0 +1,8 @@
+#ifndef _LIBSAL_H_
+#define _LIBSAL_H_
+#include <stdio.h>
+
+void example_function(void);
+
+#endif // _LIBSAL_H_
+
---
+++ Sources/libsal.c
@@ -0,0 +1,8 @@
+#include <config.h>
+#include "libSAL.h"
+
+void libSAL_function(void)
+{
+    printf("%s : %d\n", __FUNCTION__, __LINE__);
+}
+

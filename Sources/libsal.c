1d71bba1d4623580c46852214d921d70cfbff3eb
Frédéric D'HAEYER <frederic.dhaeyer@parrot.com>
2012-05-21 14:39:45 +0200

    Remove useless files.

--- Sources/libsal.c
+++
@@ -1,8 +0,0 @@
-#include <config.h>
-#include <libSAL/libsal.h>
-
-void libSAL_function(void)
-{
-    printf("%s : %d\n", __FUNCTION__, __LINE__);
-}
-
--- Sources/libsal.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LIBSAL_H_
-#define _LIBSAL_H_
-#include <stdio.h>
-
-void example_function(void);
-
-#endif // _LIBSAL_H_
-

d975528a3cf07766d680e00b1641fca7b31ede71
Frédéric D'HAEYER <frederic.dhaeyer@parrot.com>
2012-05-31 09:34:53 +0200

    Add test C file.

--- Sources/mutex.c
+++ Sources/mutex.c
@@ -19,7 +19,9 @@ int sal_mutex_init(sal_mutex_t *mutex)
 	int result = 0;
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_mutex_init((pthread_mutex_t *)mutex, NULL);
+	pthread_mutex_t *pmutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	*mutex = (sal_mutex_t)pmutex;
+	result = pthread_mutex_init((pthread_mutex_t *)*mutex, NULL);
 #endif
 
 	return result;
@@ -30,7 +32,8 @@ int sal_mutex_destroy(sal_mutex_t *mutex)
 	int result = 0;
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_mutex_destroy((pthread_mutex_t *)mutex);
+	result = pthread_mutex_destroy((pthread_mutex_t *)*mutex);
+	free(*mutex);
 #endif
 
 	return result;
@@ -41,7 +44,7 @@ int sal_mutex_lock(sal_mutex_t *mutex)
 	int result = 0;
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_mutex_lock((pthread_mutex_t *)mutex);
+	result = pthread_mutex_lock((pthread_mutex_t *)*mutex);
 #endif
 
 	return result;
@@ -52,7 +55,7 @@ int sal_mutex_trylock(sal_mutex_t *mutex)
 	int result = 0;
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_mutex_trylock((pthread_mutex_t *)mutex);
+	result = pthread_mutex_trylock((pthread_mutex_t *)*mutex);
 #endif
 
 	return result;
@@ -63,7 +66,7 @@ int sal_mutex_unlock(sal_mutex_t *mutex)
 	int result = 0;
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_mutex_unlock((pthread_mutex_t *)mutex);
+	result = pthread_mutex_unlock((pthread_mutex_t *)*mutex);
 #endif
 
 	return result;
@@ -74,7 +77,9 @@ int sal_cond_init(sal_cond_t *cond)
 	int result = 0;
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_cond_init((pthread_cond_t *)cond, NULL);
+	pthread_cond_t *pcond = (pthread_cond_t *)malloc(sizeof(pthread_cond_t));
+	*cond = (sal_cond_t)pcond;
+	result = pthread_cond_init((pthread_cond_t *)*cond, NULL);
 #endif
 
 	return result;
@@ -85,7 +90,8 @@ int sal_cond_destroy(sal_cond_t *cond)
 	int result = 0;
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_cond_destroy((pthread_cond_t *)cond);
+	result = pthread_cond_destroy((pthread_cond_t *)*cond);
+	free(*cond);
 #endif
 
 	return result;
@@ -96,7 +102,7 @@ int sal_cond_wait(sal_cond_t *cond, sal_mutex_t *mutex)
 	int result = 0;
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_cond_wait((pthread_cond_t *)cond, (pthread_mutex_t *)mutex);
+	result = pthread_cond_wait((pthread_cond_t *)*cond, (pthread_mutex_t *)*mutex);
 #endif
 
 	return result;
@@ -116,7 +122,7 @@ int sal_cond_timedwait(sal_cond_t *cond, sal_mutex_t *mutex, int time_in_ms)
 	ts.tv_nsec -= tmp * (1000 * 1000000);
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_cond_timedwait((pthread_cond_t *)cond, (pthread_mutex_t *)mutex, &ts);
+	result = pthread_cond_timedwait((pthread_cond_t *)*cond, (pthread_mutex_t *)*mutex, &ts);
 #endif
 
 	return result;
@@ -127,7 +133,7 @@ int sal_cond_signal(sal_cond_t *cond)
 	int result = 0;
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_cond_signal((pthread_cond_t *)cond);
+	result = pthread_cond_signal((pthread_cond_t *)*cond);
 #endif
 
 	return result;
@@ -138,7 +144,7 @@ int sal_cond_broadcast(sal_cond_t *cond)
 	int result = 0;
 
 #if defined(HAVE_PTHREAD_H)
-	result = pthread_cond_broadcast((pthread_cond_t *)cond);
+	result = pthread_cond_broadcast((pthread_cond_t *)*cond);
 #endif
 
 	return result;

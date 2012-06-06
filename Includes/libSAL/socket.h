67c330a195c80e3c412cd1e288ec5bfc0ce5e5fd
Frédéric D'HAEYER <frederic.dhaeyer@parrot.com>
2012-06-06 16:41:00 +0200

    Change socket abstraction layer.

--- Includes/libSAL/socket.h
+++ Includes/libSAL/socket.h
@@ -7,57 +7,8 @@
 #ifndef _SOCKET_H_
 #define _SOCKET_H_
 #include <sys/types.h>
-
-/**
- * @brief Structure used to store most addresses.
- */
-struct sockaddr;
-
-/**
- * @brief Socket address, internet style.
- */
-struct sockaddr_in;
-
-/**
- * @brief Socket domains
-*/
-typedef enum
-{
-	SAL_SOCK_DOMAIN_INET,
-	SAL_SOCK_DOMAIN_INET6,
-	SAL_SOCK_DOMAIN_MAX
-} eSAL_SOCK_DOMAIN;
-
-/**
- * @brief Socket types
-*/
-typedef enum
-{
-	SAL_SOCK_TYPE_STREAM,
-	SAL_SOCK_TYPE_DGRAM,
-	SAL_SOCK_TYPE_MAX
-} eSAL_SOCK_TYPE;
-
-/**
- * @brief Socket protocol
-*/
-typedef enum
-{
-	SAL_SOCK_PROTO_TCP,
-	SAL_SOCK_PROTO_UDP,
-	SAL_SOCK_PROTO_MAX
-} eSAL_SOCK_PROTOCOL;
-
-/**
- * @brief Socket flags
-*/
-typedef enum
-{
-	SAL_SOCK_FLAG_DONTWAIT 	= (1 << 0),
-	SAL_SOCK_FLAG_WAITALL	= (1 << 1),
-	SAL_SOCK_FLAG_NOSIGNAL  = (1 << 2),
-	SAL_SOCK_FLAG_MAX
-} eSAL_SOCK_FLAG;
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 /**
  * @brief Creates an endpoint for communication and returns a descriptor.
@@ -67,35 +18,35 @@ typedef enum
  * @param protocol A protocol to be used with the socket
  * @retval On success, a file descriptor for the new socket is returned. On error, -1 is returned, and errno is set appropriately. (See errno.h)
  */
-int sal_socket(eSAL_SOCK_DOMAIN domain, eSAL_SOCK_TYPE type, eSAL_SOCK_PROTOCOL protocol);
+int sal_socket(int domain, int type, int protocol);
 
 /**
  * @brief Initiate a connection on a socket,
- * If the socket sock is of type SOCK_TYPE_DGRAM then addr is the address to which datagrams are sent by default, and the only address from which datagrams are received.
- * If the socket is of type SAL_SOCK_TYPE_STREAM, this call attempts to make a connection to the socket that is bound to the address specified by addr.
+ * If the socket sock is of type SOCK_DGRAM then addr is the address to which datagrams are sent by default, and the only address from which datagrams are received.
+ * If the socket is of type SAL_SOCK_STREAM, this call attempts to make a connection to the socket that is bound to the address specified by addr.
  *
  * @param sockfd The socket descriptor used to connect
  * @param addr The address to connect.
  * @param addrlen The size of the addr
  * @retval On success, 0 is returned. Otherwise, -1 is returned and errno is set appropriately. (See errno.h)
  */
-int sal_connect(int sockfd, const struct sockaddr *addr, int addrlen);
+int sal_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 
 /**
  * @brief Transmit a message on a socket
- * If sal_sendto() is used on a connection-mode (SAL_SOCK_TYPE_STREAM) socket, the arguments dest_addr and addrlen are ignored (and the error EISCONN may be returned when they are not NULL and 0)
+ * If sal_sendto() is used on a connection-mode (SOCK_STREAM) socket, the arguments dest_addr and addrlen are ignored (and the error EISCONN may be returned when they are not NULL and 0)
  * Otherwise, the target address is given by dest_addr with addrlen specifying its size.
  *
  * @param sockfd The socket descriptor used to send
  * @param buf The buffer to send
  * @param buflen The buffer size
- * @param flags The bitwise OR of zero or more of the flags SAL_SOCK_FLAGS
+ * @param flags The bitwise OR of zero or more of the socket flags.
  * @param dest_addr The target address
  * @param addrlen The size of the target address
  *
  * @retval On success, 0 is returned. Otherwise, -1 is returned and errno is set appropriately. (See errno.h)
  */
-int sal_sendto(int sockfd, const void *buf, int buflen, int flags, const struct sockaddr *dest_addr, int addrlen);
+ssize_t sal_sendto(int sockfd, const void *buf, size_t buflen, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
 
 /**
  * @brief Transmit a message on a socket
@@ -103,11 +54,11 @@ int sal_sendto(int sockfd, const void *buf, int buflen, int flags, const struct 
  * @param sockfd The socket descriptor used to send
  * @param buf The buffer to send
  * @param buflen The buffer size
- * @param flags The bitwise OR of zero or more of the flags SAL_SOCK_FLAGS
+ * @param flags The bitwise OR of zero or more of the socket flags
  *
  * @retval On success, 0 is returned. Otherwise, -1 is returned and errno is set appropriately. (See errno.h)
  */
-int sal_send(int sockfd, const void *buf, int buflen, int flags);
+ssize_t sal_send(int sockfd, const void *buf, size_t buflen, int flags);
 
 /**
  * @brief Receive a message on a socket
@@ -115,14 +66,14 @@ int sal_send(int sockfd, const void *buf, int buflen, int flags);
  * @param sockfd The socket descriptor used to send
  * @param buf The buffer to fill with data received
  * @param buflen The buffer size
- * @param flags The bitwise OR of zero or more of the flags SAL_SOCK_FLAGS
+ * @param flags The bitwise OR of zero or more of the socket flags
  * @param src_addr The source address
  * @param addrlen The size of the source address
  *
  * @retval On success, the number of bytes received is returned. 0 is returned when the peer has performed an orderly shutdow.
  * Otherwise -1 is returned if an error occurred and errno is set appropriately. (See errno.h).
  */
-int sal_recvfrom(int sockfd, void *buf, int buflen, int flags, struct sockaddr *src_addr, int *addrlen);
+ssize_t sal_recvfrom(int sockfd, void *buf, size_t buflen, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
 
 /**
  * @brief Receive a message on a socket
@@ -130,12 +81,12 @@ int sal_recvfrom(int sockfd, void *buf, int buflen, int flags, struct sockaddr *
  * @param sockfd The socket descriptor used to send
  * @param buf The buffer to fill with data received
  * @param buflen The buffer size
- * @param flags The bitwise OR of zero or more of the flags SAL_SOCK_FLAGS
+ * @param flags The bitwise OR of zero or more of the socket flags
  *
  * @retval On success, the number of bytes received is returned. 0 is returned when the peer has performed an orderly shutdow.
  * Otherwise -1 is returned if an error occurred and errno is set appropriately. (See errno.h).
  */
-int sal_recv(int sockfd, void *buf, int buflen, int flags);
+ssize_t sal_recv(int sockfd, void *buf, size_t buflen, int flags);
 
 /**
  * @brief Close a socket
--- Sources/socket.c
+++ Sources/socket.c
@@ -8,189 +8,37 @@
 #include <stdlib.h>
 #include <libSAL/socket.h>
 
-#if defined(HAVE_SYS_SOCKET_H)
-#include <sys/socket.h>
-#endif
-
-#if defined(HAVE_NETINET_IN_H)
-#include <netinet/in.h>
-#endif
-
-static int sal_convert_flags(int flags)
+int sal_socket(int domain, int type, int protocol)
 {
-	int result = 0;
-
-	if(flags & SAL_SOCK_FLAG_DONTWAIT)
-	{
-#if defined(HAVE_SYS_SOCKET_H)
-		result |= MSG_DONTWAIT;
-#endif
-	}
-
-	if(flags & SAL_SOCK_FLAG_WAITALL)
-	{
-#if defined(HAVE_SYS_SOCKET_H)
-		result |= MSG_WAITALL;
-#endif
-	}
-
-	if(flags & SAL_SOCK_FLAG_NOSIGNAL)
-	{
-#if defined(HAVE_SYS_SOCKET_H)
-		result |= MSG_NOSIGNAL;
-#endif
-	}
-
-	return result;
+	return socket(domain, type, protocol);
 }
 
-static int sal_convert_types(eSAL_SOCK_TYPE type)
+int sal_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 {
-	int result = (int)SAL_SOCK_TYPE_MAX;
-
-	switch(type)
-	{
-		case SAL_SOCK_TYPE_STREAM:
-#if defined(HAVE_SYS_SOCKET_H)
-			result = SOCK_STREAM;
-#endif
-			break;
-
-		case SAL_SOCK_TYPE_DGRAM:
-#if defined(HAVE_SYS_SOCKET_H)
-			result = SOCK_DGRAM;
-#endif
-			break;
-
-		default:
-			break;
-	}
-
-	return result;
+	return connect(sockfd, addr, addrlen);
 }
 
-static int sal_convert_domains(eSAL_SOCK_DOMAIN domain)
+ssize_t sal_sendto(int sockfd, const void *buf, size_t buflen, int flags, const struct sockaddr *dest_addr, socklen_t addrlen)
 {
-	int result =(int)SAL_SOCK_DOMAIN_MAX;
-
-	switch(domain)
-	{
-		case SAL_SOCK_DOMAIN_INET:
-#if defined(HAVE_SYS_SOCKET_H)
-			result = AF_INET;
-#endif
-			break;
-
-		case SAL_SOCK_DOMAIN_INET6:
-#if defined(HAVE_SYS_SOCKET_H)
-			result = AF_INET6;
-#endif
-			break;
-
-		default:
-			break;
-	}
-
-	return result;
+	return sendto(sockfd, buf, buflen, flags, dest_addr, addrlen);
 }
 
-static int sal_convert_protocols(eSAL_SOCK_PROTOCOL protocol)
-{
-	int result =(int)SAL_SOCK_PROTO_MAX;
-
-	switch(protocol)
-	{
-		case SAL_SOCK_PROTO_TCP:
-#if defined(HAVE_SYS_SOCKET_H)
-			result = IPPROTO_TCP;
-#endif
-			break;
-
-		case SAL_SOCK_PROTO_UDP:
-#if defined(HAVE_SYS_SOCKET_H)
-			result = IPPROTO_UDP;
-#endif
-			break;
-
-		default:
-			break;
-	}
-
-	return result;
-}
-
-int sal_socket(eSAL_SOCK_DOMAIN domain, eSAL_SOCK_TYPE type, eSAL_SOCK_PROTOCOL protocol)
-{
-	int result = -1;
-	int _domain = sal_convert_domain(domain);
-	int _type = sal_convert_type(type);
-	int _protocol = sal_convert_protocols(protocol);
-
-	if( (_domain != (int)SAL_SOCK_DOMAIN_MAX) &&
-		(_type != (int)SAL_SOCK_TYPE_MAX) &&
-		(_protocol != (int)SAL_SOCK_PROTO_MAX))
-	{
-#if defined(HAVE_SYS_SOCKET_H)
-		result = socket(_domain, _type, _protocol);
-#endif
-	}
-
-	return result;
-}
-
-int sal_connect(int sockfd, const struct sockaddr *addr, int addrlen)
-{
-	int result = -1;
-
-#if defined(HAVE_SYS_SOCKET_H)
-	result = connect(sockfd, addr, (socklen_t)addrlen);
-#endif
-
-	return result;
-}
-
-int sal_sendto(int sockfd, const void *buf, int buflen, int flags, const struct sockaddr *dest_addr, int addrlen)
-{
-	int result = -1;
-	int _flags = sal_convert_flags(flags);
-
-#if defined(HAVE_SYS_SOCKET_H)
-	result = (int)sendto(sockfd, buf, (size_t)buflen, _flags, dest_addr, (socklen_t)addrlen);
-#endif
-
-	return result;
-}
-
-int sal_send(int sockfd, const void *buf, int buflen, int flags)
+ssize_t sal_send(int sockfd, const void *buf, size_t buflen, int flags)
 {
 	return sal_sendto(sockfd, buf, buflen, flags, NULL, 0);
 }
 
-int sal_recvfrom(int sockfd, void *buf, int buflen, int flags, struct sockaddr *src_addr, int *addrlen)
+ssize_t sal_recvfrom(int sockfd, void *buf, size_t buflen, int flags, struct sockaddr *src_addr, socklen_t *addrlen)
 {
-	int result = -1;
-	int _flags = sal_convert_flags(flags);
-
-#if defined(HAVE_SYS_SOCKET_H)
-	result = (int)recvfrom(sockfd, buf, (size_t)buflen, _flags, src_addr, (socklen_t *)addrlen);
-#endif
-
-	return result;
+	return recvfrom(sockfd, buf, buflen, flags, src_addr, addrlen);
 }
 
-int sal_recv(int sockfd, void *buf, int buflen, int flags)
+ssize_t sal_recv(int sockfd, void *buf, size_t buflen, int flags)
 {
-	return sal_recvfrom(sockfd, buf, buflen, flags, NULL, 0);
+	return sal_recvfrom(sockfd, buf, buflen, flags, (struct sockaddr *)NULL, (socklen_t *)NULL);
 }
 
 int sal_close(int sockfd)
 {
-	int result = -1;
-
-#if defined(HAVE_SYS_SOCKET_H)
-	result = close(sockfd);
-#endif
-
-	return result;
-
+	return close(sockfd);
 }

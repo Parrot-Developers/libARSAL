1cc6aab4895ee5bd0b5c96ee88835c14742e3dfe
Frédéric D'Haeyer <fdhaeyer@xp18a905b55a2c.parrot.biz>
2012-06-07 15:00:42 +0200

    Add test1 Xcode project

---
+++ TestBench/xcode/test1/AppDelegate.m
@@ -0,0 +1,222 @@
+//
+//  AppDelegate.m
+//  test1
+//
+//  Created by Frédéric D'Haeyer on 6/7/12.
+//  Copyright (c) 2012 Parrot SA. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#include <stdio.h>
+#include <libSAL/mutex.h>
+#include <libSAL/thread.h>
+#include <libSAL/print.h>
+
+sal_mutex_t mutex;
+sal_cond_t cond;
+int variable = 0;
+
+void *routine(void *arg)
+{
+	int i = 0;
+	SAL_PRINT(PRINT_DEBUG, "Routine started\n");
+	SAL_PRINT(PRINT_WARNING, "mutex lock\n");
+	sal_mutex_lock(&mutex);
+	variable = 1;
+	SAL_PRINT(PRINT_ERROR, "mutex signal\n");
+	sal_cond_signal(&cond);
+	SAL_PRINT(PRINT_WARNING, "mutex unlock\n");
+	sal_mutex_unlock(&mutex);
+    
+	while(i++ < 3)
+		sleep(1);
+    
+	return NULL;
+}
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+@synthesize managedObjectContext = __managedObjectContext;
+@synthesize managedObjectModel = __managedObjectModel;
+@synthesize persistentStoreCoordinator = __persistentStoreCoordinator;
+
+- (void)launchThread
+{
+	sal_thread_t thread;
+    
+	SAL_PRINT(PRINT_ERROR, "mutex init\n");
+	sal_mutex_init(&mutex);
+    
+	SAL_PRINT(PRINT_WARNING, "condition init\n");
+	sal_cond_init(&cond);
+    
+	SAL_PRINT(PRINT_WARNING, "thread create\n");
+	sal_thread_create(&thread, routine, NULL);
+    
+	SAL_PRINT(PRINT_ERROR, "Variable : %d\n", variable);
+    
+	sal_thread_join(thread, NULL);
+    
+	SAL_PRINT(PRINT_ERROR, "mutex lock\n");
+	sal_mutex_lock(&mutex);
+	SAL_PRINT(PRINT_ERROR, "mutex wait\n");
+	sal_cond_timedwait(&cond, &mutex, 1000);
+	//sal_cond_wait(&cond, &mutex);
+	SAL_PRINT(PRINT_ERROR, "mutex unlock\n");
+	sal_mutex_unlock(&mutex);
+    
+	SAL_PRINT(PRINT_DEBUG, "Variable : %d\n", variable);
+    
+	SAL_PRINT(PRINT_DEBUG, "condition destroy\n");
+	sal_cond_destroy(&cond);
+    
+	SAL_PRINT(PRINT_DEBUG, "mutex destroy\n");
+	sal_mutex_destroy(&mutex);
+    
+	SAL_PRINT(PRINT_DEBUG, "thread destroy\n");
+	sal_thread_destroy(&thread);
+}
+
+- (void)dealloc
+{
+    [_window release];
+    [__managedObjectContext release];
+    [__managedObjectModel release];
+    [__persistentStoreCoordinator release];
+    [super dealloc];
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Saves changes in the application's managed object context before the application terminates.
+    [self saveContext];
+}
+
+- (void)saveContext
+{
+    NSError *error = nil;
+    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+    if (managedObjectContext != nil) {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+             // Replace this implementation with code to handle the error appropriately.
+             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        } 
+    }
+}
+
+#pragma mark - Core Data stack
+
+// Returns the managed object context for the application.
+// If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
+- (NSManagedObjectContext *)managedObjectContext
+{
+    if (__managedObjectContext != nil) {
+        return __managedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        __managedObjectContext = [[NSManagedObjectContext alloc] init];
+        [__managedObjectContext setPersistentStoreCoordinator:coordinator];
+    }
+    return __managedObjectContext;
+}
+
+// Returns the managed object model for the application.
+// If the model doesn't already exist, it is created from the application's model.
+- (NSManagedObjectModel *)managedObjectModel
+{
+    if (__managedObjectModel != nil) {
+        return __managedObjectModel;
+    }
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"test1" withExtension:@"momd"];
+    __managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return __managedObjectModel;
+}
+
+// Returns the persistent store coordinator for the application.
+// If the coordinator doesn't already exist, it is created and the application's store added to it.
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
+    if (__persistentStoreCoordinator != nil) {
+        return __persistentStoreCoordinator;
+    }
+    
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"test1.sqlite"];
+    
+    NSError *error = nil;
+    __persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+    if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+        /*
+         Replace this implementation with code to handle the error appropriately.
+         
+         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+         
+         Typical reasons for an error here include:
+         * The persistent store is not accessible;
+         * The schema for the persistent store is incompatible with current managed object model.
+         Check the error message to determine what the actual problem was.
+         
+         
+         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
+         
+         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
+         * Simply deleting the existing store:
+         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
+         
+         * Performing automatic lightweight migration by passing the following dictionary as the options parameter: 
+         [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption, [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
+         
+         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
+         
+         */
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }    
+    
+    return __persistentStoreCoordinator;
+}
+
+#pragma mark - Application's Documents directory
+
+// Returns the URL to the application's Documents directory.
+- (NSURL *)applicationDocumentsDirectory
+{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
+@end

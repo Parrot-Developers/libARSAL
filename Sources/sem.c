cc9454dafbcdd7e0616d6cd5790b997f09807653
Nicolas Brûlez <nicolas.brulez@parrot.com>
2012-12-27 14:36:25 +0100

    Added check for sem_timedwait

--- Sources/sem.c
+++ Sources/sem.c
@@ -78,9 +78,10 @@ int sal_sem_trywait(sal_sem_t *sem)
 
 int sal_sem_timedwait(sal_sem_t *sem, const struct timespec *timeout)
 {
-    int result = -1;
+    int result = -1; /* MUST BE INIT TO -1 */
 
-#if defined(HAVE_SEMAPHORE_H)
+#if defined(HAVE_SEMAPHORE_H) && defined(HAVE_SEM_TIMEDWAIT) // Ideal case, we have a native timedwait
+
     struct timeval currentTime = {0};
     struct timespec finalTime = {0};
     gettimeofday (&currentTime, NULL);
@@ -91,6 +92,56 @@ int sal_sem_timedwait(sal_sem_t *sem, const struct timespec *timeout)
     finalTime.tv_nsec %= NSEC_TO_SEC;
 #undef NSEC_TO_SEC
     result = sem_timedwait((sem_t *)*sem, &finalTime);
+
+#elif defined(HAVE_SEMAPHORE_H) // We have a posix implementation that does not implement timedwait, do it by hand
+
+#define SEC_TO_MSEC (1000)
+#define NSEC_TO_MSEC (1000000)
+    unsigned long long msCount = (timeout->tv_sec * SEC_TO_MSEC) + (timeout->tv_nsec / NSEC_TO_MSEC) + 1; // We add one for the first wait
+#undef SEC_TO_MSEC
+#under NSEC_TO_MSEC
+    int shouldLoop = 1;
+    while (1 == shouldLoop && 0 != msCount--)
+    {
+        int err = sal_sem_trywait (sem);
+        if (0 == err)
+        {
+            // We locked the semaphore
+            shouldLoop = 0;
+            result = 0;
+        }
+        else if (EAGAIN == errno)
+        {
+            // Sleep and try again
+#define __MSEC (1000)
+            usleep (__MSEC);
+#undef __MSEC
+        }
+        else
+        {
+            // Any other error --> keep errno and return -1
+            shouldLoop = 0;
+        }
+    }
+
+    if (1 == shouldLoop)
+    {
+        /* Timeout, do a final try */
+        int err = sal_sem_trywait (sem);
+        if (0 == err)
+        {
+            /* Finally, we locked */
+            result = 0;
+        }
+        else if (EAGAIN == errno)
+        {
+            /* Still not locked, return timeout error */
+            errno = ETIMEDOUT;
+        }
+        /* No else, if we have any other error here, let */
+        /* sal_sem_trywait set errno and keep result to -1 */
+    }
+
 #endif
 
     return result;

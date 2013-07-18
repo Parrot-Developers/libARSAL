ff6642b423626132c1e52fcdff013bf85045cf54
Nicolas Brûlez <nicolas.brulez@parrot.com>
2013-07-18 10:12:17 +0200

    Added a check for min log level in debug print

--- Includes/libARSAL/ARSAL_Print.h
+++ Includes/libARSAL/ARSAL_Print.h
@@ -32,19 +32,22 @@ typedef enum
 #define ARSAL_PRINT(level, tag, format, ...)                            \
     do                                                                  \
     {                                                                   \
-        char __nowTimeStr [9];                                          \
-        struct timeval __tv;                                            \
-        struct tm __tm;                                                 \
-        gettimeofday (&__tv, NULL);                                     \
-        localtime_r (&(__tv.tv_sec), &__tm);                            \
-        strftime (__nowTimeStr, 9, "%H:%M:%S", &__tm);                  \
-        if (format[strlen (format)-1] != '\n')                          \
+        if (ARSAL_PRINT_MAX > level)                                    \
         {                                                               \
-            ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format "\n", __nowTimeStr, __tv.tv_usec / 1000, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
-        }                                                               \
-        else                                                            \
-        {                                                               \
-            ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format, __nowTimeStr, __tv.tv_usec / 1000, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+            char __nowTimeStr [9];                                      \
+            struct timeval __tv;                                        \
+            struct tm __tm;                                             \
+            gettimeofday (&__tv, NULL);                                 \
+            localtime_r (&(__tv.tv_sec), &__tm);                        \
+            strftime (__nowTimeStr, 9, "%H:%M:%S", &__tm);              \
+            if (format[strlen (format)-1] != '\n')                      \
+            {                                                           \
+                ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format "\n", __nowTimeStr, __tv.tv_usec / 1000, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+            }                                                           \
+            else                                                        \
+            {                                                           \
+                ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format, __nowTimeStr, __tv.tv_usec / 1000, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+            }                                                           \
         }                                                               \
     } while (0)
 #else

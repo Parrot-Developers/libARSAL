6aa1462f7a9b1aba61666e22ae3c14eb9b58e176
pierre eline <pierre.eline@parrot.com>
2013-07-22 13:32:18 +0200

    [Alchemy] disable shared library when TARGET_PBUILD_FORCE_STATIC is set

--- Build/atom.mk
+++ Build/atom.mk
@@ -11,6 +11,11 @@ LOCAL_DESCRIPTION := ARSDK Software Abstraction Layer
 
 LOCAL_EXPORT_LDLIBS := -larsal
 
+ifeq ("$(TARGET_PBUILD_FORCE_STATIC)","1")
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS := --disable-shared
+endif
+
+
 define LOCAL_AUTOTOOLS_CMD_POST_UNPACK
 	$(Q) cd $(PRIVATE_SRC_DIR) && ./bootstrap
 endef

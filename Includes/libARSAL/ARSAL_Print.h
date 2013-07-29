f56afee688445f6348ff55ba0e6f3a32c60850ac
Nicolas Brûlez <nicolas.brulez@parrot.com>
2013-07-29 14:43:30 +0200

    Fixed doxygen warnings + update doc

--- Includes/libARSAL/ARSAL_Print.h
+++ Includes/libARSAL/ARSAL_Print.h
@@ -26,7 +26,16 @@ typedef enum
 } eARSAL_PRINT_LEVEL;
 
 /**
- * @brief print a specific output (i.e. "[ERR] TAG | 14:30:24:245 | main:10 - My debug log")
+ * @brief Prints a specific output
+ *
+ * This is the default entry point for printing.
+ * This function outputs a string of the following format:
+ * `[LEVEL] TAG | HH:MM:SS:mmm | function:line - FORMAT`
+ *
+ * @param level The print level (eARSAL_PRINT_LEVEL enum)
+ * @param tag A short tag which will prefix the log timestamp
+ * @param format The format string to print
+ * @param ... The format parameters
  */
 #if defined(DEBUG)
 #define ARSAL_PRINT(level, tag, format, ...)                            \
@@ -75,11 +84,14 @@ typedef enum
 #endif
 
 /**
- * @brief Convert a formatted output.
+ * @brief Prints a formatted output
+ * @warning This function should not be used directly
+ * @see ARSAL_PRINT()
  *
- * @param level The level of output format
+ * @param level The level of output
+ * @param tag The tag of the output
  * @param format output format
- * @retval On success, sal_print() returns the number of characters printed. Otherwise, it returns a negative value.
+ * @retval On success, ARSAL_PrintRaw() returns the number of characters printed. Otherwise, it returns a negative value.
  */
 int ARSAL_PrintRaw(eARSAL_PRINT_LEVEL level, const char *tag, const char *format, ...);
 

32f9fd2b833380ad948f08ec7f0f0e7ef81ef1d3
Nicolas Brûlez <nicolas.brulez@parrot.com>
2013-12-26 14:41:10 +0100

    Added a generic ARSAL.h headers which includes all other ARSAL headers

---
+++ Includes/libARSAL/ARSAL.h
@@ -0,0 +1,18 @@
+/**
+ * @file libARSAL/ARSAL.h
+ * @brief Library global header for libARSAL
+ * @date 04/12/2013
+ * @author nicolas.brulez@parrot.com
+ */
+#ifndef _ARSAL_H_
+#define _ARSAL_H_
+
+#include <libARSAL/ARSAL_Endianness.h>
+#include <libARSAL/ARSAL_Mutex.h>
+#include <libARSAL/ARSAL_Print.h>
+#include <libARSAL/ARSAL_Sem.h>
+#include <libARSAL/ARSAL_Socket.h>
+#include <libARSAL/ARSAL_Thread.h>
+#include <libARSAL/ARSAL_Time.h>
+
+#endif /* _ARSAL_H_ */

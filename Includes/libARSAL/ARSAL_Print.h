ad71712fc9db96c73da88948df251e54214baf1b
Maxime Maitre <maxime.maitre@parrot.com>
2014-02-17 17:12:58 +0100

    Tick: <MAGIC> ARSAL_Print

--- Includes/libARSAL/ARSAL_Print.h
+++ Includes/libARSAL/ARSAL_Print.h
@@ -25,6 +25,8 @@ typedef enum
     ARSAL_PRINT_MAX,      /**< The maximum of enum, do not use !*/
 } eARSAL_PRINT_LEVEL;
 
+#define	ARSAL_PRINT_DATE_STRING_LENGTH 9        // HH:MM:SS\0
+
 /**
  * @brief Prints a specific output
  *
@@ -43,19 +45,19 @@ typedef enum
     {                                                                   \
         if (ARSAL_PRINT_MAX > level)                                    \
         {                                                               \
-            char __nowTimeStr [9];                                      \
+            char __nowTimeStr [ARSAL_PRINT_DATE_STRING_LENGTH];         \
             struct timeval __tv;                                        \
             struct tm __tm;                                             \
             gettimeofday (&__tv, NULL);                                 \
             localtime_r (&(__tv.tv_sec), &__tm);                        \
-            strftime (__nowTimeStr, 9, "%H:%M:%S", &__tm);              \
-            if (!strlen (format) || format[strlen (format)-1] != '\n')                      \
+            strftime (__nowTimeStr, ARSAL_PRINT_DATE_STRING_LENGTH, "%H:%M:%S", &__tm); \
+            if (!strlen (format) || format[strlen (format)-1] != '\n')  \
             {                                                           \
-                ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format "\n", __nowTimeStr, __tv.tv_usec / 1000, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+                ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format "\n", __nowTimeStr, USEC_TO_MSEC(__tv.tv_usec), __FUNCTION__, __LINE__, ##__VA_ARGS__); \
             }                                                           \
             else                                                        \
             {                                                           \
-                ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format, __nowTimeStr, __tv.tv_usec / 1000, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+                ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format, __nowTimeStr, USEC_TO_MSEC(__tv.tv_usec), __FUNCTION__, __LINE__, ##__VA_ARGS__); \
             }                                                           \
         }                                                               \
     } while (0)
@@ -65,19 +67,19 @@ typedef enum
     {                                                                   \
         if (ARSAL_PRINT_DEBUG > level)                                  \
         {                                                               \
-            char __nowTimeStr [9];                                      \
+            char __nowTimeStr [ARSAL_PRINT_DATE_STRING_LENGTH];         \
             struct timeval __tv;                                        \
             struct tm __tm;                                             \
             gettimeofday (&__tv, NULL);                                 \
             localtime_r (&(__tv.tv_sec), &__tm);                        \
-            strftime (__nowTimeStr, 9, "%H:%M:%S", &__tm);              \
-            if (!strlen (format) || format[strlen (format)-1] != '\n')                      \
+            strftime (__nowTimeStr, ARSAL_PRINT_DATE_STRING_LENGTH, "%H:%M:%S", &__tm); \
+            if (!strlen (format) || format[strlen (format)-1] != '\n')  \
             {                                                           \
-                ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format "\n", __nowTimeStr, __tv.tv_usec / 1000, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+                ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format "\n", __nowTimeStr, USEC_TO_MSEC(__tv.tv_usec), __FUNCTION__, __LINE__, ##__VA_ARGS__); \
             }                                                           \
             else                                                        \
             {                                                           \
-                ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format, __nowTimeStr, __tv.tv_usec / 1000, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+                ARSAL_PrintRaw(level, tag, "%s:%03d | %s:%d - " format, __nowTimeStr, USEC_TO_MSEC(__tv.tv_usec), __FUNCTION__, __LINE__, ##__VA_ARGS__); \
             }                                                           \
         }                                                               \
     } while (0)

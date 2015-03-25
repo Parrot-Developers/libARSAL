18960d9a8da66096324875ae1047246060caa3e7
Romain Roffé <romain.roffe@parrot.com>
2015-03-25 18:19:33 +0100

    [BUILD] Fix build with alchemy
    Change-Id: I5673c7fc4bbb5e54305974b9df078399dde897d8
    Reviewed-on: https://gerrit.parrot.biz/11921
    Reviewed-by: Nicolas Brulez <nicolas.brulez@parrot.com>

--- Build/atom.mk
+++ Build/atom.mk
@@ -16,6 +16,9 @@ ifeq ("$(TARGET_PBUILD_FORCE_STATIC)","1")
 LOCAL_AUTOTOOLS_CONFIGURE_ARGS := --disable-shared
 endif
 
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--with-sslInstallDir=$(TARGET_OUT_STAGING)/usr \
+	--with-cryptoInstallDir=$(TARGET_OUT_STAGING)/usr
 
 define LOCAL_AUTOTOOLS_CMD_POST_UNPACK
 	$(Q) cd $(PRIVATE_SRC_DIR) && ./bootstrap
